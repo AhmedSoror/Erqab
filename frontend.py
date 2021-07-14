2f35cafe5518a08e08c16d0adea79c7dc51bf2fa
AhmedSoror <ahmedsoror438@gmail.com>
2021-07-14 03:01:15 +0200

    document code

--- frontend.py
+++ frontend.py
@@ -13,16 +13,16 @@
 # -------------------------------------------------------
 # -------------------------------------------------------
 # -------------------------------------------------------
-from altair.vegalite.v4.schema.channels import Key
-import streamlit as st
-import SessionState 
 import numpy as np
 import pandas as pd
+import SessionState 
+import streamlit as st
+from solver import ReadDF
 from solver import SolverDP
 from solver import SolverMIP
-from solver import SolverGreedy
 from solver import SolverMeta
-from solver import ReadDF
+from solver import SolverGreedy
+from altair.vegalite.v4.schema.channels import Key
 
 # ----------------
 # Global variables
@@ -98,6 +98,7 @@ def SolveBulk(bulk_data, solver_select):
 # -----------------------
 # Input Component
 # -----------------------
+# get input dictionary from input areas
 def GetInputDict(data_input):
     data_dir = {str_n: data_input[str_n], str_dis: data_input[str_dis]}
     data = data_input["data"]
@@ -109,7 +110,56 @@ def GetInputDict(data_input):
 
     return data_dir
 
+# input component with input fields
+def InputComponent(id=0):
+    # place buttons on two columns
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        n = st.text_input('Total Number of users', key="n_{0}".format(id))
+    with col2:
+        distance_limit = st.text_input('Distance_Limit', key="dl_{0}".format(id))
+    # get data of n users after entering n and distance
+    if(n and distance_limit):
+        n = int(n)
+        distance_limit = int(distance_limit)
+        data_width = n
+        data_height = len(columns_name)
 
+        # initialize array to hold users data
+        data = [0]*data_height
+        for i in range(data_height):
+            data[i] = [0]*data_width
+        # data for each user
+        for usr in range(int(n)):
+            # each user in an expander
+            with st.beta_expander("users {0}".format(usr+1), expanded=False):
+                # display input areas in columns next to each other
+                cols = st.beta_columns(len(columns_name))
+                for ind, col in enumerate(cols):
+                    # store input to input array
+                    data[ind][usr] = col.text_input(label='{0}'.format(columns_name[ind], usr+1), key='{0}_{1}_{2}'.format(columns_name[ind], usr+1, id))
+                    if(data[ind][usr]):
+                        data[ind][usr] = (int)(data[ind][usr])
+
+        # solve 
+        solver_select = st.selectbox('Solver', [solver_Greedy,solver_MIP,solver_Meta,solver_DP], key="solver_select")
+        solve_but = st.button('solve')
+        if(solve_but):
+            # convert input array to input dictionary accepted by the solver
+            data_dir = GetInputDict({str_n:n,str_dis:distance_limit,"data":data})
+            # solve instance by the selected solver
+            return SolveInstance(data_dir, solver_select)
+
+# singe input component
+def SingleInputComponent(session):
+    # reset by incrementing session id
+    if st.button("Reset"):
+        session.run_id += 1
+    sol = InputComponent(session.run_id)
+    if sol:
+        OutputComponent(sol, session.run_id)
+
+# csv input
 def CSVInput(id=0):
     # # path = st.text_input('CSV file path')
     path = st.file_uploader("Choose a file",type=['csv'])
@@ -125,99 +175,7 @@ def CSVInput(id=0):
                 solutions = SolveBulk(bulk_data, solver_select)
                 if solutions:
                     OutputBulk(solutions, id)
-        return bulk_data
-        
-
-def InputComponent_old():
-    with st.beta_expander("Input", expanded=True):
-        col1, col2 = st.beta_columns(2)
-        with col1:
-            n = st.text_input('Total Number of users')
-        with col2:
-            distance_limit = st.text_input('Distance_Limit')
-
-        if(n and distance_limit):
-            n = int(n)
-            distance_limit = int(distance_limit)
-
-            data_width = n
-            data_height = len(columns_name)
-
-            data = [0]*data_height
-            for i in range(data_height):
-                data[i] = [0]*data_width
-
-            # user i    :  max  , min_fare  , min_capacity , capacity , location_x    , location_y
-            form = st.form(key='Input_Form')
-            with form:
-                cols = form.beta_columns(len(columns_name))
-                for ind, col in enumerate(cols):
-                    # col.write("as")
-                    for usr in range(int(n)):
-                        data[ind][usr] = col.text_input(
-                            '{0}_{1}'.format(columns_name[ind], usr+1))
-                        if(data[ind][usr]):
-                            data[ind][usr] = (int)(data[ind][usr])
-                        # col.selectbox('{0}'.format(columns_name[ind]), [i for i in range(10)], key='{0}_{1}'.format(columns_name[ind],k+1))
-
-            submit = form.form_submit_button('Submit')
-
-            if submit:
-                data_dir = GetInputDict({str_n:n,str_dis:distance_limit,"data":data})
-                sol = SolverMIP(data_dir)
-                return sol
-
-def InputComponent(id=0):
-    col1, col2 = st.beta_columns(2)
-    with col1:
-        n = st.text_input('Total Number of users', key="n_{0}".format(id))
-    with col2:
-        distance_limit = st.text_input('Distance_Limit', key="dl_{0}".format(id))
-
-    if(n and distance_limit):
-        n = int(n)
-        distance_limit = int(distance_limit)
-        data_width = n
-        data_height = len(columns_name)
-
-        data = [0]*data_height
-        for i in range(data_height):
-            data[i] = [0]*data_width
-
-        for usr in range(int(n)):
-            with st.beta_expander("users {0}".format(usr+1), expanded=False):
-                cols = st.beta_columns(len(columns_name))
-                for ind, col in enumerate(cols):
-                    data[ind][usr] = col.text_input(label='{0}'.format(columns_name[ind], usr+1), key='{0}_{1}_{2}'.format(columns_name[ind], usr+1, id))
-                    if(data[ind][usr]):
-                        data[ind][usr] = (int)(data[ind][usr])
-                    # col.selectbox('{0}'.format(columns_name[ind]), [i for i in range(10)], key='{0}_{1}'.format(columns_name[ind],k+1))
-
-        # m = st.markdown("""
-        # <style>
-        # div.stButton > button:first-child {
-        #     background-color: rgb(70, 187, 26);
-        # }
-        # </style>""", unsafe_allow_html=True)
-
-        
-        solver_select = st.selectbox('Solver', [solver_Greedy,solver_MIP,solver_Meta,solver_DP], key="solver_select")
-
-        solve_but = st.button('solve')
-        if(solve_but):
-            # df = pd.DataFrame(data,columns=(columns_name))
-            # st.dataframe(df)
-            data_dir = GetInputDict({str_n:n,str_dis:distance_limit,"data":data})
-            return SolveInstance(data_dir, solver_select)
-
-def SingleInputComponent(session):
-    if st.button("Reset"):
-        session.run_id += 1
-    sol = InputComponent(session.run_id)
-    if sol:
-        OutputComponent(sol, session.run_id)
-
-
+            return bulk_data
 
 
 
@@ -227,16 +185,20 @@ def SingleInputComponent(session):
 # Output Component
 # -----------------------
 def OutputComponent(data, id=0):
+    # dispaly total number of travellers
     st.write("Traverllers: {0}".format(data["z"]))
     # display cars
     data_1 = {"Cars":data[str_cars]}
     df = pd.DataFrame.from_dict(data_1)
-        # drop rows with only 0s
+     
+    # drop rows with only 0s
     # a_series = (df != 0).any(axis=1)
     # df_noZeros = df.loc[a_series]
+    
+    # display matchings
     st.write('Matchings: ',df, Key="asd{0}".format(id))
-    # st.dataframe(df)
 
+# output multiple solutions
 def OutputBulk(solutions, id=0):
     for sol in solutions:
         OutputComponent(sol, id)    
@@ -258,15 +220,14 @@ def main(id=0):
     st.title("Erqab")
     # session id used for reset
     session = SessionState.get(run_id=0)
-    
-    sidebar = st.sidebar.radio("page",["Single Input", "Upload CSV"],1)
+    # create a side bar to switch between single input and bulk input
+    sidebar = st.sidebar.radio("page",["Single Input", "Upload CSV"],0)
     if(sidebar == "Single Input"):
         SingleInputComponent(session)
     elif (sidebar == "Upload CSV"):
-        # increment session id to reset component
+        # increment session id to reset single input component
         session.run_id += 1
         bulk_data = CSVInput()
-        # print("L256_ session: {0}".format(session.run_id))
                 
 
         
